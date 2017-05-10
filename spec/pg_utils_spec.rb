f7b8ea7e98596518c5b5735b68de80c20156df83
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-09 23:09:37 -0300

    change config file dir path

--- spec/pg_utils_spec.rb
+++ spec/pg_utils_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe PgUtils do
   end
 
   it "has a default config file path" do
-    expect(PgUtils::Config::DEFAULTS).to eq({config_file: "pg_utils.yml"})
+    expect(PgUtils::Config::DEFAULTS).to eq({config_file: "config/pg_utils.yml"})
   end
 end
